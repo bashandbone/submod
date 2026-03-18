be8e404f04fd21d2c1f3e3b26c60505b5264ae6e
Adam Poulemanos <89049923+bashandbone@users.noreply.github.com>
2026-03-18 13:10:11 -0400

    Update PowerShell completion script path

--- src/long_abouts.rs
+++ src/long_abouts.rs
@@ -4,7 +4,7 @@
 
 //! A series of multiline strings for long-about text. We put them here to keep the command module somewhat readable.
 
-pub const COMPLETE_ME: &str = r#"
+pub const COMPLETE_ME: &str = r##"
 Generates shell completion script for the specified shell.
 
 Supported shells:
@@ -25,9 +25,9 @@ Usage:
 
     - Fish: `submod completeme fish > ~/.config/fish/completions/submod.fish`
 
-    - PowerShell: `submod completeme powershell > ~/.config/powershell/completions/submod.ps1` or `submod completeme pwsh > ~/.config/powershell/completions/submod.ps1`
-
+    - PowerShell: `submod completeme powershell > $Home\Documents\PowerShell\completions\submod.completion.ps1`
+    
     - Zsh: `submod completeme zsh > ~/.zsh/completions/_submod` or `submod completeme zsh > ~/.zfunc/_submod`
 
     - Nushell: `submod completeme nu > "$NUSHELL_CONFIG_DIR/scripts/completions/submod.nu" && echo 'use completions/submod.nu' >> "$NU_CONFIG_PATH"`
-"#;
+"##;
